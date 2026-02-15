a26181105ddd7dec496185c3c52a53999f8c31d6
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-02-15 07:48:28 +0700

    distreg With (In) R Software

--- distreg/distreg.R
+++ distreg/distreg.R
@@ -3,10 +3,8 @@
 # IG : @olahdatasemarang_
 # Distribution regression as in Foresi and Peracchi (1995) Use distreg With (In) R Software
 install.packages("remotes")
-remotes::install_github("zengying17/ate_pct-r")
-install.packages("sandwich")
-library("sandwich")
-library("atepct")
+remotes::install_github("bcallaway11/distreg")
+library("distreg")
 # Estimate Distribution regression as in Foresi and Peracchi (1995) Use distreg With (In) R Software
 distreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/distreg/main/distreg/distreg.csv", sep = ";")
 y0 <- median(distreg$lcfincome)
